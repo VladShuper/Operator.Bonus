3dd949f983174ae3994ac07dc8717921184460b3
Vlad Shuper <cristianbox777@gmail.com>
2024-01-17 17:24:23 +0300

    newintB

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int cashplus = 700;
+        int cashplus = 100;
         int sum = 0;
         int bonus = 0;
         if (cashplus > 1000) {
             sum = ((cashplus + balance) / 100 + (cashplus + balance));
-            bonus = (cashplus + balance) / 100;
+            bonus = (cashplus + balance) / 100 - 1;
         }
-        System.out.println("Ваш баланс состовляет:" + (cashplus + balance + bonus) + "руб.");
+        System.out.println("Ваш баланс состовляет:" + (cashplus + balance + bonus ) + "руб.");
         System.out.println("Вам начислен бонус в размере:" + bonus);
     }
 }
