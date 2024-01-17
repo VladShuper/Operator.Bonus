e5589c61135814e8d621547dee2caeb8f6af5e7f
Vlad Shuper <cristianbox777@gmail.com>
2024-01-17 11:14:44 +0300

    newpravka

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int balans;
-        int cashplus;
         int bonus;
-        balans = 100;
-        cashplus = 1100;
+        int balans = 100;
+        int cashplus = 500;
         if (cashplus > 1000) {
             bonus = 1;
         } else {
             bonus = 0;
+            int cash = (cashplus + balans);
         }
-
-        int cash = cashplus / 100 * bonus + balans + cashplus;
+        int cash = (cashplus + balans) / 100 + bonus - 1 + (cashplus + balans);
 
         System.out.println("Итоговая сумма с бонусом:" + cash);
     }
